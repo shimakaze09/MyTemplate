c0ff51e91670e358ae6e07e234eabb0d23dbc49e
John <yangtianzhuo970425@gmail.com>
2024-12-24 22:39:31 +1300

    Rename instantiation checks for clarity; update related test cases and functionality in TypeList and TemplateList

--- src/test/02_1_TemplateList/main.cpp
+++ src/test/02_1_TemplateList/main.cpp
@@ -120,15 +120,17 @@ int main() {
        << "\t" << TInstantiable_v<list0, B<>> << endl;
   cout << "TInstantiable<list0, D>" << endl
        << "\t" << TInstantiable_v<list0, D<>> << endl;
-  cout << "TInstantiableList_v<list0, TemplateList<TypeList<void>, A, B>>"
+  cout << "TCanInstantiateToList_v<list0, TemplateList<TypeList<void>, A, B>>"
        << endl
        << "\t"
-       << TInstantiableList_v<list0, TInstanceList_t<TemplateList<A, B>,
-                                                     TypeList<void>>> << endl;
-  cout << "TInstantiableList_v<list0, TemplateList<TypeList<void>, D, B>>"
+       << TCanInstantiateToList_v<
+              list0,
+              TInstanceList_t<TemplateList<A, B>, TypeList<void>>> << endl;
+  cout << "TCanInstantiateToList_v<list0, TemplateList<TypeList<void>, D, B>>"
        << endl
        << "\t"
-       << TInstantiableList_v<list0, TInstanceList_t<TemplateList<D, B>,
-                                                     TypeList<void>>> << endl;
+       << TCanInstantiateToList_v<
+              list0,
+              TInstanceList_t<TemplateList<D, B>, TypeList<void>>> << endl;
   return 0;
 }
