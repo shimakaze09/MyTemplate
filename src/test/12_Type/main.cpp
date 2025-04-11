c451fa7cdd37ddc54fe480cd51442c2415f673dc
John <yangtianzhuo970425@gmail.com>
2025-04-12 10:52:51 +1200

    Add type traits and static assertions for Type class in main.cpp

--- src/test/12_Type/main.cpp
+++ src/test/12_Type/main.cpp
@@ -9,11 +9,16 @@
 using namespace My;
 using namespace std;
 
+struct A {};
+
 int main() {
   std::cout << Type_of<int>.GetName() << std::endl;
   std::cout << Type_of<float>.GetName() << std::endl;
   std::cout << Type_of<int>.GetTypeID().GetValue() << std::endl;
   std::cout << Type_of<float>.GetTypeID().GetValue() << std::endl;
+
+  static_assert(Type_of<A> == Type{"A"});
+  static_assert(Type_of<const int&>.RemoveCVRef() == Type_of<int>);
 
   if (Type_of<int> < Type_of<float>)
     cout << "Type_of<int> <  Type_of<float>" << endl;
